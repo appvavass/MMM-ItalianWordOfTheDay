87ec0460317651f6ae6787dd556b97379427c42f
appvavass <appvavass@gmail.com>
2024-04-01 18:07:14 +0200

    new fix

--- wordoftheday.js
+++ wordoftheday.js
@@ -1,35 +1,37 @@
-// modules/translated-word/translated-word.js
-
 Module.register('MMM-ItalianWordOfTheDay', {
-	word: '',
+    initialWord: 'ciaone',
+    word: 'ciaone',
   
-	start: function() {
-	  this.getTranslatedWord();
-	  // Refresh the word every 30 seconds
-	  setInterval(() => {
-		this.getTranslatedWord();
-	  }, 30000);
-	},
+    start: function() {
+        var self = this;
+        // Update the DOM initially with the initial word
+        this.updateDom();
+        // Make an AJAX request to retrieve the translated word
+        this.getTranslatedWord();
+        // Refresh the word every 30 seconds
+        setInterval(() => {
+            self.getTranslatedWord();
+        }, 30000);
+    },
   
-	getTranslatedWord: function() {
-	  var self = this;
-	  var xhr = new XMLHttpRequest();
-	  xhr.open('GET', 'query.py', true);
-	  xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-		  var response = JSON.parse(xhr.responseText);
-		  self.word = response.word;
-		  self.updateDom();
-		}
-	  };
-	  xhr.send();
-	},
+    getTranslatedWord: function() {
+        var self = this;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'query.py', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                self.word = response.word;
+                self.updateDom();
+            }
+        };
+        xhr.send();
+    },
   
-	getDom: function() {
-	  const wrapper = document.createElement('div');
-	  wrapper.className = 'MMM-ItalianWordOfTheDay';
-	  wrapper.innerHTML = this.word;
-	  return wrapper;
-	}
-  });
-  
+    getDom: function() {
+        const wrapper = document.createElement('div');
+        wrapper.className = 'MMM-ItalianWordOfTheDay';
+        wrapper.innerHTML = "Initial Word: " + this.initialWord + "<br>New Word: " + this.word;
+        return wrapper;
+    }
+});
